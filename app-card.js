6ff3b92b2f24eb227d69592942d01a9836c32fbb
“eenfeeneet” <burnhafiz@gmail.com>
2019-10-13 08:09:13 +0800

    updated card component

--- app-card.js
+++ app-card.js
@@ -1,55 +1,44 @@
-class NavBar extends HTMLElement {
+const template = document.createElement('template');
+template.innerHTML = `
+    <style>
+    .card {
+      -webkit-box-shadow: 0px 10px 13px -7px #000000, 0px 0px 20px 10px rgba(111,128,138,0.5); 
+      box-shadow: 0px 10px 13px -7px #000000, 0px 0px 20px 10px rgba(111,128,138,0.5);
+      background: #0069BF;
+      border: 5px solid #6F808A;
+      border-radius: 5px;
+      margin: 1rem auto;
+      height: 100%;
+      weight: 100%;
+      text-align: center;
+      font-family: 'Noto Sans JP', sans-serif;
+    }
+    </style>
+    <div class="card">
+        <h3></h3>
+    </div>
+`;
+class Card extends HTMLElement {
   constructor() {
     super();
-    var shadow = this.attachShadow({ mode: 'open' });
 
-    var style = document.createElement('style');
+    this._shadowRoot = this.attachShadow({ mode: 'open' });
+    this._shadowRoot.appendChild(template.content.cloneNode(true));
 
-    style.textContent = `
-    .topnav {
-      background-color: #027770;
-      border: 2px solid #04998F;
-      border-radius: 5px;
-      height: 3em;
-      padding: 2px;
-    }
-    
-    .topnav a {
-      float: left;
-      display: block;
-      color: #00BFB2;
-      text-align: center;
-      padding: 14px 16px;
-      text-decoration: none;
-      font-size: 1rem;
-      box-sizing: border-box;
-    }
-    
-    .topnav a:hover {
-      color: black;
-    }`;
+    this.$content = this._shadowRoot.querySelector('h3');
+  }
+  connectedCallback() {
+    this.renderCard();
+  }
 
-    var navbar = document.createElement('div');
-    navbar.setAttribute('class', 'topnav');
-    navbar.setAttribute('id', 'myTopnav');
-
-    // var homeLink = document.createElement('a');
-    // homeLink.setAttribute('class', 'active');
-    // homeLink.setAttribute('href', '#home');
-    // homeLink.textContent = 'home';
-
-    var newsLink = document.createElement('a');
-    newsLink.setAttribute('href', '#news');
-    newsLink.textContent = 'Trello-Clone';
-
-    var info = document.createElement('span');
-    info.setAttribute('class', 'info');
-
-    shadow.appendChild(style);
-    shadow.appendChild(navbar);
-
-    navbar.appendChild(newsLink);
+  static get properties() {
+    return {
+      content
+    };
+  }
+  renderCard() {
+    this.$content.textContent = this.content;
   }
 }
 
-customElements.define('app-navbar', NavBar);
+customElements.define('app-card', Card);
